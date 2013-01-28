017cf8c778e856bbe8d28b0637f51de76185e45d
Oliver Schrenk <oliver.schrenk@gmail.com>
2013-01-28 12:56:20 +0100

    Remove #hashCode and #equals methods

--- src/main/java/com/acme/rhino/Modifier.java
+++ src/main/java/com/acme/rhino/Modifier.java
@@ -18,39 +18,4 @@ public class Modifier {
 	public int getValue() {
 		return value;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((message == null) ? 0 : message.hashCode());
-		result = prime * result + value;
-		return result;
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final Modifier other = (Modifier) obj;
-		if (message == null) {
-			if (other.message != null) {
-				return false;
-			}
-		} else if (!message.equals(other.message)) {
-			return false;
-		}
-		if (value != other.value) {
-			return false;
-		}
-		return true;
-	}
-
 }
